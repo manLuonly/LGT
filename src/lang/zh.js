442f72339cc26c641bbd502bee15aa28e65072d1
LuGuangZong <1454118276@qq.com>
2019-11-12 21:58:29 +0800

    'init'

---
+++ src/lang/zh.js
@@ -0,0 +1,50 @@
+const zh = {
+    // layout
+    commons: {
+        xiaoai: '小爱',
+        admin: '管理员',
+        editor: '赵晓编',
+        quit: '退出',
+        hi: '您好',
+        index: '首页',
+        userManage: '用户管理',
+        // share: '分享功能',
+        projectManagement: '项目管理',
+        lookCase: '查看案例',
+        addCase: '添加案例',
+        clientManagement: '客户管理',
+        projectStatistics: '项目统计',
+        userInfo: '客户信息',
+        incomePayPosition: '收支分布',
+        permission: '权限设置',
+        pagePer: '页面权限',
+        directivePer: '按钮权限',
+        serviceProject: '服务项目',
+        addProject: '添加项目',
+        loginOut: '退出',
+        infoManage: '信息管理',
+        personalInfo: '个人信息',
+        infoShow1: '个人信息子菜单1',
+        infoShow2: '个人信息子菜单2',
+        infoShow3: '个人信息子菜单3',
+        infoShow4: '个人信息子菜单4',
+        infoShow5: '个人信息子菜单5',
+        updateInfo: '修改信息',
+        infoModify1: '修改信息子菜单1',
+        infoModify2: '修改信息子菜单2',
+        infoModify3: '修改信息子菜单3',
+        // wechatNumber: '微信号'
+    },
+    index: {
+        yearLoss: '年度总盈亏',
+        yearProfit: '年度收益率',
+        potentialInvestor: '潜在投资人',
+        intentionInvestor: '意向投资人',
+        waitExamineInvestor: '待审投资人',
+        examiningInvestor: '审核中投资人',
+        tenMillion: '千万元',
+        person: '人'
+    }
+}
+
+export default zh;
